43a350c5536c740579c7a4964ee887dd45387727
dvsobel <Dani@daniellbelsMBP2.wireless.newschool.edu>
2016-11-18 10:15:57 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -4,39 +4,44 @@
 // alert("this is a function!");
 
 //boolean to toggle states
-var pcontact_showing = true;
+// var pcontact_showing = true;
 
 //add event listener to element, event is click, displayElement is the function that will
 //be called when the user clicks on the element
-document.getElementById("pcontact").addEventListener("click", display_second_element);
+// document.getElementById("pcontact").addEventListener("click", display_second_element);
 
 
 //function is being defined here- display element
-function display_second_element(){
+// function display_second_element(){
 
 	//if else statement to determine behavior 
 	//if the paragraph is showing, do something
 	//else, do something else
-	if(pcontact_showing == true){
-		document.getElementById("pcontact_info").style.display="none";
+// 	if(pcontact_showing == true){
+// 		document.getElementById("pcontact_info").style.display="none";
 
-		pcontact_showing = false;
-	}
-	else{
-		document.getElementById("pcontact_info").style.display="inherit";
+// 		pcontact_showing = false;
+// 	}
+// 	else{
+// 		document.getElementById("pcontact_info").style.display="inherit";
 		
 
-		pcontact_showing = true;
+// 		pcontact_showing = true;
 
-	}	
+// 	}	
 
-}
+// }
 
-//fly in for design
-  
-  $(document).ready(function(){
-                setTimeout(function() {
-                    $('.fly-in-text').removeClass('hidden');
-                }, 500);
-                
-            })();
+
+// how to link to another page
+document.getElementById("dessert").addEventListener("click",function(){
+    window.location.href="painting.html";
+});
+
+document.getElementById("dairy").addEventListener("click",function(){
+    window.location.href="design.html";
+});
+
+document.getElementById("meat").addEventListener("click",function(){
+    window.location.href="aboutMe.html";
+});
